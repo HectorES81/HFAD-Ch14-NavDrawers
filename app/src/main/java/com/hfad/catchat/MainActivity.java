d57a3a3fc3825667276affd94540a1fba9f53951
Hector Chicas <hect0rchicas@hotmail.com>
2021-05-09 16:42:47 -0400

    Page 605 Test Drive

--- app/src/main/java/com/hfad/catchat/MainActivity.java
+++ app/src/main/java/com/hfad/catchat/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hfad.catchat;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import java.util.function.ToLongBiFunction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame, fragment);
+        ft.commit();
     }
 }
